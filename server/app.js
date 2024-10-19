609e213de4a033b2d56754f5020cda080ee82964
Alexoswim <oswinalex1@gmail.com>
2024-10-19 18:38:49 +0530

    helmet lib

--- server/app.js
+++ server/app.js
@@ -1,38 +1,48 @@
 const Express = require('express');
 const location = require('./mapCollection');
-const app = Express();
+const helmet = require('helmet');
 const cors = require('cors');
 
-app.use(Express.json());
-app.use(Express.urlencoded({extended: true}))
-app.use(cors())
+const app = Express();
 
-app.get("/", cors(), (req, res) => {
-    // Define your root route logic here
-    res.json("Welcome to the server!");
-  });
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Use Helmet to set Content Security Policy
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    fontSrc: ["'self'", "https://smart-blind-stick-4.onrender.com"],
+    scriptSrc: ["'self'"], // Adjust as needed
+  }
+}));
+
+app.get("/", (req, res) => {
+  // Define your root route logic here
+  res.json("Welcome to the server!");
+});
+
 app.post("/location", async (req, res) => {
-    const {lat, long,id} = req.body;
-   
-   
-    
-    const details = {
-     
-      lat:lat,
-      long:long,
-      id:id,
-    };
-    try {
-      await location.insertMany([details]);
-      res.json("done");
-  
-    } catch (e) {
-      res.json("server error  " );
-      console.error("Error:", e);
-      
-    }
-  });
-  
-  app.listen(8000, () => {
-    console.log("Server is running on port 8000");
-  });
+  const { lat, long, id } = req.body;
+
+  const details = {
+    lat: lat,
+    long: long,
+    id: id,
+  };
+
+  try {
+    await location.insertMany([details]);
+    res.json("done");
+  } catch (e) {
+    res.json("server error");
+    console.error("Error:", e);
+  }
+});
+
+// Start the server
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
